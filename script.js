c71bd05fe8bc59083a031f537ed9d11fc1c27f91
Sarthak Rana <rsarthak2845@gmail.com>
2025-01-04 12:36:45 +0530

    added files

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    VANTA.FOG({
+        el: "#vanta-bg",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        highlightColor: 0xc31432,
+        midtoneColor: 0x240b36,
+        lowlightColor: 0x2900ff,
+        baseColor: 0x000000,
+    });
+});
