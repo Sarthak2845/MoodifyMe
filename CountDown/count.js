35ee9ac9b64f090534c8a3a5f39ecb4d8ba3e080
Sarthak Rana <rsarthak2845@gmail.com>
2025-01-05 12:47:14 +0530

    removed vanta js and added footer

--- CountDown/count.js
+++ CountDown/count.js
@@ -1,19 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Initialize Vanta.js Background
-    if (typeof VANTA !== 'undefined' && VANTA.FOG) {
-        VANTA.FOG({
-            el: "#vanta-bg",
-            mouseControls: true,
-            touchControls: true,
-            gyroControls: false,
-            minHeight: 900.00,
-            minWidth: 300.00,
-            highlightColor: 0xc31432,
-            midtoneColor: 0x240b36,
-            lowlightColor: 0x2900ff,
-            baseColor: 0x000000,
-        });
-    }
     // Flatpickr Initialization
     if (typeof flatpickr !== 'undefined') {
         flatpickr("#deadline", {
